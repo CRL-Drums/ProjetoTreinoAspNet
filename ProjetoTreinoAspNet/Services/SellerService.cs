a93756f7ec33f6185515261f75a51db2ff65c74d
Cláudio Lima <crl.drums@gmail.com>
2021-07-26 23:28:36 -0300

    Service Folder created

---
+++ ProjetoTreinoAspNet/Services/SellerService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ProjetoTreinoAspNet.Data;
+using ProjetoTreinoAspNet.Models;
+
+namespace ProjetoTreinoAspNet.Services
+{
+    public class SellerService
+    {
+//CRL Lindo
+        private readonly ProjetoTreinoAspNetContext _context;
+
+        public SellerService(ProjetoTreinoAspNetContext context)
+        {
+            _context = context;
+        }
+
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();
+        }
+    }
+}
