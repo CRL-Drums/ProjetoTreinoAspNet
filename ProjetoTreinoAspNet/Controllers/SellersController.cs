8477e5a07695e0d1086fb14e93ac71be0359a687
Cláudio Lima <crl.drums@gmail.com>
2021-07-31 12:08:46 -0300

    Added validator form

--- ProjetoTreinoAspNet/Controllers/SellersController.cs
+++ ProjetoTreinoAspNet/Controllers/SellersController.cs
@@ -40,6 +40,12 @@ namespace ProjetoTreinoAspNet.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Seller seller)
         {
+            if (!ModelState.IsValid)
+            {
+                var departments = _departmentService.FindAll();
+                var viewModel = new SellerFormViewModel { Seller = seller, Departments = departments };
+                return View(viewModel);
+            }
             _sellerservice.Insert(seller);
             return RedirectToAction(nameof(Index));
         }
@@ -104,10 +110,18 @@ namespace ProjetoTreinoAspNet.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Edit(int id, Seller seller)
         {
+            if (!ModelState.IsValid)
+            {
+                var departments = _departmentService.FindAll();
+                var viewModel = new SellerFormViewModel { Seller = seller, Departments = departments };
+                return View(viewModel);
+            }
+
             if (id != seller.Id)
             {
                 return RedirectToAction(nameof(Error), new { message = "Id mismatch!" });
             }
+
             try
             {
                 _sellerservice.Update(seller);
